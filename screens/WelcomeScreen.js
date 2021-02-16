460dd9038a6676e634257e6b5e1effb51a70fe59
Elir-mahad <mmahad280@gmail.com>
2021-02-15 23:30:36 -0500

    improved css

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -13,23 +13,33 @@ const WelcomeScreen = ({ navigation }) => {
 	//
 	return (
 		<KeyboardAvoidingView style={styles.container}>
-			<StatusBar style="light" />
-			<Image
-				source={require("../assets/samurai.png")}
-				style={{ width: 200, height: 200 }}
-			/>
-
-			<Text style={styles.titleText}> Hagakure</Text>
 			{/*  */}
-			<Text style={styles.bodyText}> This is a quote </Text>
-
+			<StatusBar style="light" />
+			{/*  */}
+			<Image
+				style={styles.image}
+				source={require("../assets/hagakurelogo.png")}
+			/>
+			{/*  */}
+			<Text style={styles.titleText}> Hagakure 葉隠</Text>
+			{/*  */}
+			<Text style={styles.subtitleText}> Yamamoto Tsunetomo </Text>
+			{/*  */}
+			<Text style={styles.bodyText}>
+				Written during a time when there was no officially sanctioned samurai
+				fighting, the book grapples with the dilemma of maintaining a warrior
+				class in the absence of war and reflects the author's nostalgia for a
+				world that had disappeared before he was born.
+			</Text>
+			{/*  */}
 			<TouchableOpacity
 				//
 				style={styles.button}
 				onPress={() => navigation.navigate("Table of Contents")}
 			>
-				<Text style={styles.buttonText}>Click Me</Text>
+				<Text style={styles.buttonText}>Table of Contents</Text>
 			</TouchableOpacity>
+			{/*  */}
 		</KeyboardAvoidingView>
 	);
 };
@@ -40,18 +50,35 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: "center",
 		justifyContent: "center",
-		padding: 10,
+		paddingLeft: 10,
+		paddingRight: 10,
 		backgroundColor: "white"
 	},
 	titleText: {
+		fontSize: 30,
+		paddingTop: 5,
+		paddingBottom: 5
+	},
+	subtitleText: {
 		fontSize: 20,
 		paddingTop: 5,
 		paddingBottom: 5
 	},
 	bodyText: {
 		fontSize: 20,
-		paddingTop: 5,
-		paddingBottom: 5
+		padding: 5,
+		marginLeft: 20,
+		marginRight: 20,
+		marginBottom: 10,
+		textAlign: "center",
+		borderRadius: 5,
+		borderColor: "black",
+		borderWidth: 2
+	},
+	image: {
+		width: 200,
+		height: 200,
+		borderRadius: 100
 	},
 	button: {
 		paddingRight: 10,
