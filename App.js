c4bd12e3a43b627fc557beae7b5675387327385d
Elir-mahad <mmahad280@gmail.com>
2021-02-17 08:30:08 -0500

    changed the architecture of the code. Instead of having all the screens in app.js, i put all the screens in a new filed called mainstacknavigator and then i imported mainstacknavigator into app.js. I am doing this because i want to have multiple ways for traversing the screens. The stack navigator is one way to do this, but there are other options such as the drawer and tab navigator. To incorporate these other otpions into the app i need to keep the app.js as clean as possible. Right now everything seems to be working well.

--- App.js
+++ App.js
@@ -6,16 +6,12 @@ import React, { useState } from "react";
 // below imports are from react navigation
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 
 // below are the different screen components
 import WelcomeScreen from "./screens/WelcomeScreen";
-import TableOfContentsScreen from "./screens/TableOfContentsScreen";
-import EduOneScreen from "./screens/EduOneScreen";
-import EduTwoScreen from "./screens/EduTwoScreen";
+import MainStackNavigator from "./screens/MainStackNavigator";
 
 // below are the necessary imports for custom fonts
-
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 
@@ -57,38 +53,9 @@ export default function App() {
 		);
 	}
 
-	//--- Next we wil declare the global screen options and initialize the createStackNaviagtor
-
-	const globalScreenOptions = {
-		// The constant globalScreenOptions stores the below css styles
-		headerStyle: { backgroundColor: "darkred" },
-		headerTitleStyle: { color: "white" },
-		headerTintiColor: "white"
-	};
-
-	const Stack = createStackNavigator();
-	// The contsant Stack is going to store all the screens
-
 	return (
 		<NavigationContainer>
-			<Stack.Navigator screenOptions={globalScreenOptions}>
-				<Stack.Screen
-					name="Welcome" // Give the component a name
-					component={WelcomeScreen} // Import the component
-				/>
-				<Stack.Screen
-					name="Table of Contents" // Give the component a name
-					component={TableOfContentsScreen} // Import the component
-				/>
-				<Stack.Screen
-					name="Education 1" // Give the component a name
-					component={EduOneScreen} // Import the component
-				/>
-				<Stack.Screen
-					name="Education 2" // Give the component a name
-					component={EduTwoScreen} // Import the component
-				/>
-			</Stack.Navigator>
+			<MainStackNavigator />
 		</NavigationContainer>
 	);
 }
---
+++ screens/MainStackNavigator.js
@@ -0,0 +1,49 @@
+//! ------------------------------------------------- IMPORTS
+// below are foundational imports that come with the app
+import React from "react";
+
+// below imports are from react navigation
+import { createStackNavigator } from "@react-navigation/stack";
+// import "react-native-gesture-handler";
+
+// below are the different screen components
+import WelcomeScreen from "./WelcomeScreen";
+import TableOfContentsScreen from "./TableOfContentsScreen";
+import EduOneScreen from "./EduOneScreen";
+import EduTwoScreen from "./EduTwoScreen";
+
+//!--- Declare the global screen options and initialize the createStackNaviagtor
+
+const globalScreenOptions = {
+	// The constant globalScreenOptions stores the below css styles
+	headerStyle: { backgroundColor: "darkred" },
+	headerTitleStyle: { color: "white" },
+	headerTintiColor: "white"
+};
+
+const Stack = createStackNavigator();
+
+const MainStackNavigator = () => {
+	return (
+		<Stack.Navigator screenOptions={globalScreenOptions}>
+			<Stack.Screen
+				name="Welcome" // Give the component a name
+				component={WelcomeScreen} // Import the component
+			/>
+			<Stack.Screen
+				name="Table of Contents" // Give the component a name
+				component={TableOfContentsScreen} // Import the component
+			/>
+			<Stack.Screen
+				name="Education 1" // Give the component a name
+				component={EduOneScreen} // Import the component
+			/>
+			<Stack.Screen
+				name="Education 2" // Give the component a name
+				component={EduTwoScreen} // Import the component
+			/>
+		</Stack.Navigator>
+	);
+};
+
+export default MainStackNavigator;
